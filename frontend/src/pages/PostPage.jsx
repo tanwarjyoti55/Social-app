7038eff408d707a336f6cb6844370236330928c8
tanwarjyoti55 <jyotitanwar20141@gmail.com>
2024-03-18 00:05:19 +0530

    create a app

---
+++ frontend/src/pages/PostPage.jsx
@@ -0,0 +1,59 @@
+import { Avatar, Box, Button, Divider, Flex, Image, Text } from "@chakra-ui/react";
+import Actions from "../components/Actions";
+import Comment from "../components/Comment";
+import { useState } from "react";
+
+const PostPage = () => {
+const[liked, setLiked]=useState(false);
+	return (
+		<>
+			<Flex>
+				<Flex w={"full"} alignItems={"center"} gap={3}>
+					<Avatar src='/zuck-avatar.png' size={"md"} name='Mark Zuckerberg' />
+					<Flex>
+						<Text fontSize={"sm"} fontWeight={"bold"}>
+							markzuckerberg
+						</Text>
+						<Image src='/verified.png' w='4' h={4} ml={4} />
+					</Flex>
+				</Flex>
+			</Flex>
+
+			<Text my={3}>Let's talk about Threads.</Text>
+
+		
+				<Box borderRadius={6} overflow={"hidden"} border={"1px solid"} borderColor={"gray.light"}>
+					<Image src='/post1.png' w={"full"} />
+				</Box>
+		
+
+			<Flex gap={3} my={3}>
+				<Actions liked={liked} setLiked={setLiked} />
+			</Flex>
+
+      <Flex gap={2} alignItems={'center'}>
+        <Text color={'gray.light'} fontSize={'sm'}>238 replies</Text>
+        <Box w={0.5} h={0.5} borderRadius={'full'} bg={'gray.light'}></Box>
+        <Text color={'gray.light'} fontSize={'sm'}>{200 + (liked?1:0)} likes</Text>
+      </Flex>
+
+			<Divider my={4} />
+
+			<Flex justifyContent={"space-between"}>
+				<Flex gap={2} alignItems={"center"}>
+					<Text fontSize={"2xl"}>👋</Text>
+					<Text color={"gray.light"}>Get the app to like, reply and post.</Text>
+				</Flex>
+				<Button>Get</Button>
+			</Flex>
+
+			<Divider my={4} />
+      <Comment comment='Looks Good' createdAt='1d' likes={200} username={'Mark Zukerberge'} profilePic={'/post1.png'}/>
+      <Comment comment='Amazing' createdAt='3d' likes={100} username={'Johndeo'} profilePic={'/post2.png'}/>     
+      <Comment comment='Ohh looks very pretty' createdAt='6d' likes={800} username={'Mark Zukerberge'} profilePic={'/post3.png'}/>
+			
+		</>
+	);
+};
+
+export default PostPage;
---
+++ frontend/src/pages/UserPage.jsx
@@ -0,0 +1,18 @@
+import UserHeader from "../components/UserHeader";
+import UserPost from "../components/UserPost";
+
+const UserPage = () => {
+
+
+	return (
+		<>
+    <UserHeader/>
+    <UserPost postImg={'/post1.png'} postTitle={'This is my first post'} likes={234} replies={123} />
+    <UserPost postImg={'/post2.png'} postTitle={'Hello Sunshine'} likes={546} replies={23} />
+    <UserPost postImg={'/post3.png'} postTitle={'Hey Everyone'} likes={100} replies={2} />
+    <UserPost  postTitle={'Hey Guys'} likes={100} replies={2} />
+		</>
+	);
+};
+
+export default UserPage;
